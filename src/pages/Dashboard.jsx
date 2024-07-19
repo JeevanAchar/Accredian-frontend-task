05cf217acfa77cd18e4a78b02a4a95e12464513b
JeevanAchar <jeevanachar83@gmail.com>
2024-07-19 20:23:34 +0530

    implemented promo page

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import Promo from "../components/Promo";
 
 function Dashboard() {
     return (
         <div>
             <Navbar/>
+            <Promo/>
         </div>
     );
 }
