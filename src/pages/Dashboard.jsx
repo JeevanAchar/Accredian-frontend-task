934f286f26230b44d9c7a7cad1b9046f08787e19
JeevanAchar <jeevanachar83@gmail.com>
2024-07-19 22:09:06 +0530

    implemented referal benefits

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Promo from "../components/Promo";
+import ReferralBenefit from "../components/ReferralBenefit";
 
 function Dashboard() {
     return (
         <div>
-            <Navbar/>
-            <Promo/>
+            <Navbar />
+            <Promo />
+            <ReferralBenefit />
         </div>
     );
 }
