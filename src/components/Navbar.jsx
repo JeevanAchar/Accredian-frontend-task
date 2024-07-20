f1c54d5abcc8278449dedb8e80485df4045778ea
JeevanAchar <jeevanachar83@gmail.com>
2024-07-21 03:16:23 +0530

    fixed navbar issue

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,39 +1,44 @@
 import React from "react";
-import { FaAngleDown } from "react-icons/fa";
+import { FaBars, FaTimes } from "react-icons/fa";
 import Logo from "../asset/Accredian.png";
 import Button from "./ui/button";
 
 function Navbar() {
+    const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
     return (
-        <section className="sticky top-0 border-b z-50 bg-white">
-            <div className="w-full min-h-[60px] 2xl:max-w-7xl mx-auto flex justify-between items-center">
-                <div className="flex items-center gap-4">
-                    <img src={Logo} alt="Logo" className="max-w-32" />
-                    <div className="flex justify-between items-center max-w-28 bg-blue-600 text-white px-4 py-2 rounded-md font-medium" role="button">
-                        <span>
-                            Courses
-                        </span>
-                        <FaAngleDown />
+        <section>
+            <nav className="bg-white fixed w-full z-50 top-0 start-0 border-b border-gray-200">
+                <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                    <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+                        <img src={Logo} className="h-8" alt="Logo" />
+                        <Button>Course</Button>
+                    </a>
+                    <div className="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+                        <Button>Login</Button>
+                        <button onClick={() => setIsMenuOpen(!isMenuOpen)} type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-sticky" aria-expanded="false">
+                            <span className="sr-only">Open main menu</span>
+                            {isMenuOpen ? <FaTimes size={20} /> : <FaBars size={20} />}
+                        </button>
                     </div>
-                </div>
-                <div className="flex items-center justify-end gap-4">
-                    <div>
-                        <ul className="flex items-center gap-6 text-sm">
-                            <li className="cursor-pointer">Refer & Earn</li>
-                            <li className="cursor-pointer">Resources</li>
-                            <li className="cursor-pointer">About us</li>
+                    <div className={`items-center justify-between ${isMenuOpen ? "flex" : "hidden"} w-full md:flex md:w-auto md:order-1`} id="navbar-sticky">
+                        <ul className="w-full flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white">
+                            <li>
+                                <a href="/" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0">Refer & Earn</a>
+                            </li>
+                            <li>
+                                <a href="/" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0">Resources</a>
+                            </li>
+                            <li>
+                                <a href="/" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0">About Us</a>
+                            </li>
+                            <li>
+                                <a href="/" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0">Courses</a>
+                            </li>
                         </ul>
                     </div>
-                    <div className="flex items-center gap-4">
-                        <Button type="secondary">
-                            Login
-                        </Button>
-                        <Button>
-                            try for free
-                        </Button>
-                    </div>
                 </div>
-            </div>
+            </nav>
         </section>
 
     );
